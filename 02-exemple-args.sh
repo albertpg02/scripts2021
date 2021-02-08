ab2b230681c97a88070631df380fb7d9b0d763c4
isx45832602 <isx45832602@j10.informatica.escoladeltreball.org>
2021-02-08 13:48:25 +0100

    script 01

---
+++ 02-exemple-args.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#albert
+#m01 asix
+#febrer 2021
+# ------------------
+echo '$*:' $*
+echo '$@:' $@
+echo '$#:' $#
+echo '$0:' $0
+echo '$1:' $1
+echo '$2:' $2
+echo '$3:' $3
+echo '$4:' $4
+echo '$5:' $5
+echo '$6:' $6
+echo '$7:' $7
+echo '$8:' $8
+echo '$9:' $9
+echo '$10:' ${10}
+echo '$11:' ${11}
+nom='albert'
+echo "${nom} pujadas"
+exit 0
